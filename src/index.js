52870562fd7d979b2ceabdb0b503b5ccc97f9715
EtoZheLamia <Andariel2175@yandex.ru>
2023-02-11 19:08:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const regex = /[^0-9]/g
+    let result = n.toString().split('').reverse().toString().replaceAll(regex, '')
+    return Number(result)
 }
